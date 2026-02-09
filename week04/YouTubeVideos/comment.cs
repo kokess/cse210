f8729fd2abdc51ea00c8a01b34e5e1542ac4ec37
kokess <aessien5@byupathway.edu>
2026-02-09 19:17:47 +0100

    commit

---
+++ week04/YouTubeVideos/comment.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+
+public class Comment
+{
+    public string CommenterName { get; private set; }
+
+    public string Text { get; private set; }
+
+    public Comment(string commenterName, string text)
+    {
+        CommenterName = commenterName;
+        Text = text;
+    }
+}
